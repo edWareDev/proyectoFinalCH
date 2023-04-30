26bef3628a4a057fce0586d75417a07ef8d6a470
Edward Espinoza <em.esc.et@gmail.com>
2023-04-30 17:10:55 -0500

    endpoin get updated

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Segunda entrega del proyectoFinalCH
 
-Revisar:
----
-* En el endpoint ./ Se puede usar los parametras limit, page, sort y query en el endponit.
-    * Ejemplo: http://localhost:8080/?limit=5&page=2&sort=des
-    * Ejemplo 2: http://localhost:8080/?limit=5&page=2&query=productStatus:true&sort=asc
-    
+## Revisar:
 
+- Corregí para que el endpoint get entregue los datos solicitados
+  - Ejemplo: http://localhost:8080/api/products?limit=2&page=1
+- En el endpoint ./ Se puede usar los parametras limit, page, sort y query en el endponit.
+  - Ejemplo: http://localhost:8080/?limit=5&page=2&sort=des
+  - Ejemplo 2: http://localhost:8080/?limit=5&page=2&query=productStatus:true&sort=asc
--- src/controllers/products.get.controller.js
+++ src/controllers/products.get.controller.js
@@ -3,16 +3,53 @@ import { productsManager } from '../dao/mongoose.products.manager.js';
 export async function getProductsController(req, res) {
     const queryParams = req.query;
     const allProducts = await productsManager.getProducts();
-    const resultado = [];
+    const products = await productsManager.getProductsWithFilters(queryParams.limit, queryParams.page, queryParams.query, queryParams.sort)
+    const totalItems = allProducts.length;
+    const totalPages = Math.ceil(totalItems / queryParams.limit)
+    const page = Number(queryParams.page) || 1
+    let prevPage;
+    let hasPrevPage;
+    let nextPage;
+    let hasNextPage;
+    if (page >= 2) { prevPage = page - 1; hasPrevPage = true } else { prevPage = null; hasPrevPage = false }
+    if (page == totalPages) { nextPage = null; hasNextPage = null } else { nextPage = page + 1; hasNextPage = true }
 
-    if (queryParams.limit) {
-        for (let product = 0; product <= queryParams.limit - 1; product++) {
-            if (allProducts[product]) {
-                resultado.push(allProducts[product]);
+    const currentUrl = `${req.protocol}://${req.get('host')}/api/products`;
+    let nextPageUrl = `${currentUrl}?`;
+    let prevPageUrl = `${currentUrl}?`;
+    for (const key in queryParams) {
+        if (Object.hasOwnProperty.call(queryParams, key)) {
+            const element = queryParams[key];
+            if (key == 'page') {
+                if (hasNextPage === true) {
+                    nextPageUrl = nextPageUrl + `${key}=${nextPage}&`
+                } else { nextPageUrl = null }
+                if (hasPrevPage === true) {
+                    prevPageUrl = prevPageUrl + `${key}=${prevPage}&`
+                } else { prevPageUrl = null }
+            } else {
+                if (hasNextPage === true) {
+                    nextPageUrl = nextPageUrl + `${key}=${element}&`
+                } else { nextPageUrl = null }
+                if (hasPrevPage === true) {
+                    prevPageUrl = prevPageUrl + `${key}=${element}&`
+                } else { prevPageUrl = null }
             }
         }
-        res.json(resultado);
-    } else {
-        res.json(allProducts);
     }
+    console.log(nextPageUrl);
+    const finalResult = {
+        status: 'success',
+        payload: products,
+        totalPages,
+        prevPage,
+        nextPage,
+        page: page,
+        hasPrevPage,
+        hasNextPage,
+        prevLink: prevPageUrl,
+        nextLink: nextPageUrl
+    }
+
+    res.json(finalResult)
 }
